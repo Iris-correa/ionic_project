45422537f65e820a3ff3f2417867c505f4ad767c
iris-correa <iris555.correa@yahoo.com>
2022-08-18 21:31:35 -0300

    Operações em typeScript

--- APP1/src/app/home/home.page.ts
+++ APP1/src/app/home/home.page.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  result;
+
+   SUM(formu) {
+    let num1 = formu.value.n1;
+    let num2 = formu.value.n2;
+    
+    this.result = parseFloat(num1)+parseFloat(num2)
+  }
+SUB(formu){
+  let num1 = formu.value.n1;
+  let num2 = formu.value.n2;
+  this.result = parseFloat(num1)-parseFloat(num2)
+}
+MULT(formu){
+  let num1 = formu.value.n1;
+  let num2 = formu.value.n2;
+  this.result = parseFloat(num1)*parseFloat(num2)
+}
+DIV(formu){
+  let num1 = formu.value.n1;
+  let num2 = formu.value.n2;
+  this.result = parseFloat(num1)/parseFloat(num2)
+}
 
 }
